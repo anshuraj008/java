f3074dd0a1f410ba47873f6c2fe17daf5a07f466
0888888888 <anshuraj9905151156@gmail.com>
2025-04-26 10:06:31 +0530

    commit

---
+++ variables/program1.java
@@ -0,0 +1,30 @@
+
+public class program1 {
+    public static void main(String[] args) {
+        
+        int myNumber = 600;
+
+        int yourNumber;
+        yourNumber = 400;
+        System.out.println(yourNumber);
+        yourNumber = 40;
+        System.out.println(yourNumber);
+
+        boolean isRainy = true;
+        System.out.println(isRainy);
+
+        String myName = "Anshu Raj";
+        System.out.println(myName);
+
+        char myGrade = 'A';
+        System.out.println(myGrade);
+
+        float myFloat = 10.5f;
+        System.out.println(myFloat);
+
+        double myDouble = 10.5;
+        System.out.println(myDouble);
+        System.out.println(myDouble + 10.5);
+ 
+    }    
+}
