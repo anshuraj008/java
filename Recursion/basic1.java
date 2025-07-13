fcefeebe50079c31ee8b3a020cc9d552a40611e8
0888888888 <anshuraj9905151156@gmail.com>
2025-07-14 01:18:32 +0530

    ggg

---
+++ Recursion/basic1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class basic1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Welcome to factorial  generator\n");
+
+        System.out.println("Please enter the number to generate factorial: ");
+        int num = input.nextInt();
+
+        long fact = factorial(num);
+        // long fact = factorialIterative(num);
+        System.out.println("Factorial of " + num + " is " + fact);
+    }
+
+    //Recursive function
+    public static long factorial(int num){
+        System.out.println("Function call: " + num);
+        
+        if(num == 1){
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+
+    //loop function
+    // public static long factorialIterative(int num) {
+    //     long result = 1;
+    //     for (int i = 1; i <= num; i++) {
+    //         result *= i;
+    //     }
+    //      return result;
+    // } 
+}
