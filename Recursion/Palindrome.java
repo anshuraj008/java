17f7b12500d6db81788c1b985025ec222cd262cf
0888888888 <anshuraj9905151156@gmail.com>
2025-07-22 22:02:49 +0530

    hhhhfsa

---
+++ Recursion/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Welcome to Palindrome Checker Program\nEnter a word: ");
+        String word = input.next();
+        System.out.println("Your String is " + 
+                            ((isPalindrome(word) ? "Palindrone"
+                                                  : "Not Palindrone")));
+    }
+
+    public static boolean isPalindrome(String word) {
+        if (word.length() <= 1) {
+            return true;
+        }
+        int lastPos = word.length() - 1;
+        if(word.charAt(0) != word.charAt(lastPos)) {
+            return false;
+        }
+        return isPalindrome(word.substring(1, lastPos));
+    }
+}
