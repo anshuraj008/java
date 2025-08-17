d01f59008a65e53af8dfb581c8216d29d3481b19
0888888888 <anshuraj9905151156@gmail.com>
2025-08-17 23:34:59 +0530

    ghhh

---
+++ BitManipulation/Basic.java
@@ -0,0 +1,11 @@
+
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println((5 & 6));  //101 & 110 = 100
+        System.out.println((5 | 6));  //101 & 110 = 111
+        System.out.println((5 ^ 6));  //101 ^ 110 = 011
+        System.out.println(~5);
+        System.out.println(5<<2);
+    }
+}
--- Recursion/Palindrome1.java
+++ Recursion/Palindrome1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome {
+public class Palindrome1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Welcome to Palindrome Checker Program\nEnter a word: ");
