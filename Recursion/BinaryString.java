e9dc432c23ec4d215a3a02718842dc5463dbd467
0888888888 <anshuraj9905151156@gmail.com>
2025-09-05 02:43:27 +0530

    Add BinaryString and NoOfPairs classes with recursive implementations

---
+++ Recursion/BinaryString.java
@@ -0,0 +1,27 @@
+public class BinaryString {
+    public static void main(String[] args) {
+        printBinaryString(3, 0, "");
+    }
+
+    public static void printBinaryString(int n, int lastPlace, String str) {
+        //base case
+        if(n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        // if(lastPlace == 0){
+        //     //sit 0 on chair n
+        //     printBinaryString(n-1, 0, str.append("0"));
+        //     printBinaryString(n-1, 1, str.append("1"));
+        // } else{
+        //     //sit 1 on chair n
+        //     printBinaryString(n-1, 0, str.append("0"));
+        // }
+
+         printBinaryString(n-1, 0, str+"0");
+         if(lastPlace == 0) {
+            printBinaryString(n-1, 1, str+"1");
+         }
+    }
+}
