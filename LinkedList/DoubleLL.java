ecb46dde020d7c0fddae8414c4d62848ab6c3bd6
0888888888 <anshuraj9905151156@gmail.com>
2025-09-17 21:34:43 +0530

    Implement Double Linked List (DoubleLL) with methods for adding, removing, printing, and reversing nodes; include main method for demonstration.

---
+++ LinkedList/DoubleLL.java
@@ -0,0 +1,130 @@
+public class DoubleLL {
+    public  class Node {
+        int data; 
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public int size;
+
+    //add first
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode; 
+        head.prev = null;
+    }
+
+    //add last
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        tail.next = null;
+    }
+
+    //print
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //remove first
+    public int removeFirst(){
+        if(head == null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    //remove last
+    public int removeLast(){
+        if(head == null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    //reverse DLL
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.addLast(4);
+        dll.addLast(5);
+        dll.print();
+        System.out.println(dll.size);
+
+        dll.removeFirst();
+        dll.removeLast();
+        dll.print();
+        System.out.println(dll.size);
+
+        dll.reverse();
+        dll.print();
+    }
+}
