fb6cb3b53ec6614871f77b87e2f0e30652246943
0888888888 <anshuraj9905151156@gmail.com>
2025-10-24 21:39:38 +0530

    Add HeightCal class with methods to calculate height, count, sum, and diameter of a binary tree; implement inner Node and Info classes.

---
+++ BinaryTree/HeightCal.java
@@ -0,0 +1,93 @@
+public class HeightCal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root){ //O(n)
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static int count(Node root){ //O(n)
+        if(root == null){
+            return 0;
+        }
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+        return leftCount + rightCount + 1;
+    }
+
+    public static int sum(Node root){ //O(n)
+        if(root == null){
+            return 0;
+        }
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
+    public static int diameter1(Node root){ //O(n^2)
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int leftDiameter = diameter1(root.left);
+        int rightDiameter = diameter1(root.right);
+
+        int selfDiameter = leftHeight + rightHeight + 1;
+
+        return Math.max(selfDiameter, Math.max(leftDiameter, rightDiameter));
+    }
+
+    static class Info{
+        int diameter;
+        int height;
+
+        public Info(int diameter, int height){
+            this.diameter = diameter;
+            this.height = height;
+        }
+    }
+
+    public static Info diameter2(Node root){ //O(n)
+        if(root == null){
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+
+        int diameter = Math.max(leftInfo.height + rightInfo.height + 1, Math.max(leftInfo.diameter, rightInfo.diameter));
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+
+        return new Info(diameter, height);
+    }
+
+    public static void main(String[] args) { 
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        System.out.println(height(root));
+        System.out.println(count(root));
+        System.out.println(sum(root));
+
+        System.out.println(diameter1(root));
+        System.out.println(diameter2(root).diameter);
+    }
+}
