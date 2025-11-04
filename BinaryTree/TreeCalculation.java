0413ac38568409205699ca36a1cf30012e3f73a4
0888888888 <anshuraj9905151156@gmail.com>
2025-11-05 01:35:53 +0530

    Add BST class with insert, delete, search, and traversal methods; enhance TreeCalculation with LCA, distance, and ancestor functionalities.

--- BinaryTree/TreeCalculation.java
+++ BinaryTree/TreeCalculation.java
@@ -14,7 +14,7 @@ public class TreeCalculation {
     }
 
     public static int height(Node root){ //O(n)
-        if(root == null){
+        if(root == null){    
             return 0;
         }
         int leftHeight = height(root.left);
@@ -163,6 +163,147 @@ public class TreeCalculation {
         System.out.println();
     }
 
+    //Kth level
+    public static void KLevel(Node root, int level, int k){
+        if(root == null){
+            return;
+        }
+        if(level == k){
+            System.out.print(root.data+" ");
+            return;
+        }
+        KLevel(root.left, level + 1, k);
+        KLevel(root.right, level + 1, k);
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+        if(root == null){
+            return false;
+        }
+
+        path.add(root);
+        if(root.data == n){
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight){
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2){ //O(n)
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        //LCA
+        int i = 0;
+        while(i < path1.size() && i < path2.size()){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+            i++;
+        }
+        //last equal node -> i-1th index
+        return path1.get(i - 1);
+    }
+
+    public static Node lca2(Node root, int n1, int n2){ //O(n)
+        if(root == null){
+            return null;
+        }
+        if(root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        Node leftLCA = lca2(root.left, n1, n2);
+        Node rightLCA = lca2(root.right, n1, n2);
+
+        //leftLCA=val rightLCA = null
+        if(leftLCA == null){
+            return rightLCA;
+        }
+        if(rightLCA == null){
+            return leftLCA;
+        }
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        } else if(leftDist == -1){
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        } 
+    }
+
+    public static int minDist(Node root, int n1, int n2){
+        Node lcaNode = lca2(root, n1, n2);
+        int dist1 = lcaDist(lcaNode, n1);
+        int dist2 = lcaDist(lcaNode, n2);
+        return dist1 + dist2;
+    }
+
+    public static int KAncestor(Node root, int n, int k){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = KAncestor(root.left, n, k);
+        int rightDist = KAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+        int max = Math.max(leftDist, rightDist);
+        if(max + 1 == k){
+            System.out.println("Kth Ancestor: " + root.data);
+        }
+        return max + 1;
+    }
+
+     public static int transform(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+     }
+
+     public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+     }
+
     public static void main(String[] args) {
         //Binary Tree Construction
         Node root = new Node(1);
@@ -187,5 +328,24 @@ public class TreeCalculation {
         System.out.println(isSubtree(root, subRoot));
 
         topView(root); 
+
+        //Kth level
+        int k = 3;
+        KLevel(root, 1, k);
+
+        Node lcaNode = lca(root, 4, 6);
+        System.out.println("\nLCA: " + lcaNode.data);
+
+        Node lcaNode2 = lca2(root, 4, 5);
+        System.out.println("LCA2: " + lcaNode2.data);
+
+        int n1 = 4, n2 = 6;
+        System.out.println("Min dist: " + minDist(root, n1, n2));
+
+        int n = 5, K = 2;
+        KAncestor(root, n, K); 
+
+        transform(root);
+        preorder(root);
     }
 }
