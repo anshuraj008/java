633217128cd1eed433ee314ce0e3a5409488cfaf
0888888888 <anshuraj9905151156@gmail.com>
2026-01-14 23:34:48 +0530

    Add maxHeap and minHeap classes with basic heap operations

---
+++ Heap/maxHeap.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+public class maxHeap {
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data){
+            // add at last index
+            arr.add(data);
+
+            int x = arr.size() - 1; // child index
+            int par = (x - 1) /2; // parent index
+
+            while(arr.get(x) > arr.get(par)){ // max heap condition 0(logn)
+                // swap
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par)); // set method is used to update value at index
+                arr.set(par, temp); // swap complete
+
+                x = par;
+                par = (x - 1) /2;
+            }
+        }
+
+        public int peek(){
+            return arr.get(0); // top element
+        }
+
+        // fix the heap property
+        private void heapify(int i){
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            int maxIdx = i;
+
+                if(left < arr.size() && arr.get(maxIdx) < arr.get(left)){
+                    maxIdx = left;
+                }
+
+            if(right < arr.size() && arr.get(maxIdx) < arr.get(right)){
+                maxIdx = right;
+            }
+
+            if(maxIdx != i){
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(maxIdx));
+                arr.set(maxIdx, temp);
+
+                heapify(maxIdx);
+            }
+        }
+
+        public int remove(){
+            int data = arr.get(0);
+
+            // step 1: swap first and last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            // step 2: remove last
+            arr.remove(arr.size()-1);
+
+            // step 3: heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty(){
+            return arr.size() == 0;
+        }
+    }
+    public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while(!h.isEmpty()){
+            System.out.println(h.peek());
+            h.remove();
+        }
+    }
+}
