29af9a49187cda132f27a903f03c7f089915be93
0888888888 <anshuraj9905151156@gmail.com>
2026-03-01 01:27:44 +0530

    Add PrefixProb class to implement a trie with insert and prefix counting functionalities

---
+++ Tries/PrefixProb.java
@@ -0,0 +1,52 @@
+public class PrefixProb {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node(){
+            for(int i=0; i<children.length; i++){
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int i=0; i<word.length(); i++){
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static int countWordsWithPrefix(String prefix){
+        Node curr = root;
+        for(int i=0; i<prefix.length(); i++){
+            int idx = prefix.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                return 0;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.freq;
+    }
+
+    public static void main(String[] args) {
+        String arr[] = {"zebra", "dog", "duck", "dove"};
+        for(int i=0; i<arr.length; i++){
+            insert(arr[i]);
+        }
+
+        String prefix = "dog";
+        System.out.println("Words with prefix '" + prefix + "': " + countWordsWithPrefix(prefix));
+    }
+}
